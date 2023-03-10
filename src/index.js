4f16d51ea6df249dbe4c005953a2113834ea680f
Christopher James Leo <aungaungoo.hotmail@gmail.com>
2023-03-11 00:00:32 +0630

    finish project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import $ from 'jquery';
 import './index.css';
-import {navItems,bannerBtns,bannerLinks,counters} from "./navbar-item";
+import {navItems,bannerBtns,bannerLinks,counters,whatwedos,recentWorks,logos,contentInfos,forminputs,footerItems} from "./items";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
@@ -121,7 +122,7 @@ function WeAreGlint(){
     </section>
   )
   function Counter(props){
-    console.log(props);
+    // console.log(props);
     const {title,count} = props;
     return (
       <div className="col-lg-3 col-md-6 col-sm-12 counter-content-container">
@@ -149,11 +150,255 @@ function WhatWeDo(){
           </div>
         </div>
         {/* End section title */}
+        <div className='whatwedo-content-container'>
+          <div className='row'>
+            {
+              whatwedos.map(function(whatwedo){
+                return (
+                  <Whatwedoitems key={whatwedo.id} whatwedo = {whatwedo}/>
+                )
+              })
+            }
+          </div>
+        </div>
       </div>
 
     </section>
   )
+  function Whatwedoitems(props){
+    const {icon,title,para} = props.whatwedo;
+    return (
+      <div className='col-lg-6 col-md-12 d-flex justify-content-between align-items-start'>
+        <div className='wedoicons' style={{"color" : "var(--primary-global-color)"}}><i className={icon}></i></div>
+        <div className='text-start wedo-content'>
+          <h1>{title}</h1>
+          <p className='text-muted mt-5'>{para}</p>
+        </div>
+
+      </div>
+    )
+  }
 }
+
+function Recentwork(){
+  return (
+    <section className='py-5 recentwork-section'>
+      <div className='container'>
+        {/* start section title */}
+        <div className='text-center section-title-container'>
+          <div className='row'>
+            <div className='col-12 mx-auto section-title'>
+              <h5 className=' text-uppercase fw-semibold' style={{"color" : "var(--primary-global-color)"}}>RECENT WORKS</h5>
+              <h1 className='text-dark'>We’ve got everything you need to launch and grow your business</h1>
+              <hr className='mt-5'/>
+            </div>
+          </div>
+        </div>
+        {/* End section title */}
+        <div className='row works-main-container'>
+          {recentWorks.map(function(recentWork){
+            return(
+              <Worksitems key={recentWork.id} {...recentWork}/>
+            )
+          })}
+        </div>
+      </div>
+
+    </section>
+  )
+  function Worksitems(props){
+    const {img,title,type} = props;
+    // console.log(img,title,type);
+    return (
+      <div className='col-6 p-0 m-0 work-img-container'>
+        <div className='p-3 d-flex flex-column justify-content-end align-items-start  work-img-overlayer'>
+          <a href='#' className='d-flex justify-content-center align-items-center flex-column img-link'>
+            <div className='link-roap'></div>
+            <div className='link-icon'><i class="fas fa-link"></i></div>
+          </a>
+          <div className='text-center img-plus-icon'>
+            <span className='d-inline-block'><i class="fas fa-plus"></i></span>
+          </div>
+          <div className='overlayer-content'>
+            <h4 className="text-white">{title}</h4>
+            <h5 className=''>{type}</h5>
+          </div>
+        </div>
+        <div className='work-img'>
+          <img src={img} width="100%" alt={img}/>
+        </div>
+      </div>
+    )
+  }
+}
+
+function OurClients(){
+  return (
+    <section className='py-5 ourclients-section'>
+      <div className='container'>
+        {/* start section title */}
+        <div className='text-center section-title-container'>
+          <div className='row'>
+            <div className='col-12 mx-auto section-title'>
+              <h5 className=' text-uppercase fw-semibold' style={{"color" : "var(--primary-global-color)"}}>OUR CLIENTS</h5>
+              <h1 className='text-dark'>Glint has been honored to partner up with these clients</h1>
+              <hr className='mt-5'/>
+            </div>
+          </div>
+        </div>
+        {/* End section title */}
+      </div>
+      <div id='client-logos' className='client-logos'>
+        <ul id='logo-slider' className='logo-slider row list-unstyled'>
+          {
+            logos.map(function(logo){
+              return (
+
+                  <Clients key = {logo.id} {...logo}/>
+
+
+              )
+            })
+          }
+        </ul>
+      </div>
+    </section>
+  )
+  function Clients(props){
+    const {img, alt} = props;
+    // console.log(img,alt);
+    return (
+      <li className='col-lg-2 col-md-3 col-sm-4'>
+        <img src={img} width="100%" alt={alt}/>
+      </li>
+    )
+  }
+}
+
+function ContactUs(){
+  return(
+    <section className='py-5 contactus-section'>
+      <div className='container'>
+        {/* start section title */}
+        <div className='text-center section-title-container'>
+          <div className='row'>
+            <div className='col-12 mx-auto section-title'>
+              <h5 className=' text-uppercase fw-semibold' style={{"color" : "var(--primary-global-color)"}}>CONTACT US</h5>
+              <h1 className='text-white'>Reach out for a new project or just say hello</h1>
+              <hr className='mt-5'/>
+            </div>
+          </div>
+        </div>
+        {/* End section title */}
+        <div className='row p-5 form-container'>
+          <div className='col-lg-8 col-md-12 p-3'>
+            <h6 className='text-uppercase mb-5'>send us a message</h6>
+            <form className='contact-form'>
+              {
+                forminputs.map(function(forminput){
+                  return (
+                    <div className='form-group mb-3'>
+                      <Inputs key={forminput.id} {...forminput} />
+                    </div>
+                  )
+                })
+              }
+              <div className='form-group mb-3'>
+                <textarea name='message' id="message" className='form-control' placeholder='Your Message' ></textarea> 
+
+              </div>
+              <div className='d-grid'>
+                <input type={'submit'} value="submit" className='text-uppercase btn '/>
+              </div>
+              
+            </form>
+          </div>
+          <div className='col-lg-4 col-md-12 p-3'>
+            <h6 className='text-uppercase mb-5'>send us a message</h6>
+            <div className='content-item'>
+              {
+                contentInfos.map(function(contentInfo){
+                  return (
+                    <Contentitem key={contentInfo.id} {...contentInfo} />
+                  )
+                })
+              }
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </section>
+    
+  )
+  function Inputs(props){
+    const {type,name,inputid,placeholder} = props;
+    return(
+      <input type={type} name={name} id={inputid} className="form-control" placeholder={placeholder}  autoComplete='off' />
+    )
+  }
+  function Contentitem(props){
+    return(
+      <div className="content-info-item-container">
+        <div className='content-info-item'>
+          <h5 className='text-uppercase mb-2' style={{"color" : "var(--primary-global-color )"}}>{props.title}</h5>
+          <p>{props.content}</p>
+          <a href='#' className='email-links'>{props.emailone}</a>
+          <a href='#' className='email-links'>{props.emailtwo}</a>
+          <p>{props.phone}</p>
+          <p>{props.mobile}</p>
+          <p>{props.fax}</p>
+          <a href="#"className='d-inline-block contact-links'><i className={props.facebook}></i></a>
+          <a href="#"className='d-inline-block contact-links'><i className={props.twitter}></i></a>
+          <a href="#"className='d-inline-block contact-links'><i className={props.instargram}></i></a>
+          <a href="#"className='d-inline-block contact-links'><i className={props.youtube}></i></a>
+        </div>
+        
+      </div>
+    )
+  }
+}
+
+
+function Footer(){
+  return (
+    <footer className='footer-section'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-lg-6 col-md-12 mb-3 p-3'>
+              <img src="https://preview.colorlib.com/theme/glint/images/logo-footer.png" width="70px" className='mb-5' alt="logo"/>
+              <p>
+                "Proin eget tortor risus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Nulla porttitor accumsan tincidunt. Nulla porttitor accumsan tincidunt. Quaerat voluptas autem necessitatibus vitae aut
+              </p>
+    
+          </div>
+          <div className='col-lg-6 col-md-12 mb-3 p-3'>
+              <h6 className='mb-5'>Get Notified</h6>
+              <p>
+                Quia quo qui sed odit. Quaerat voluptas autem necessitatibus vitae aut non alias sed quia. Ut itaque enim optio ut excepturi deserunt iusto porro.
+              </p>
+              <form className='footer-input'>
+                <div className='form-group-container d-flex'>
+                  <div className='form-group email-input'>
+                    <input type={"email"} name="emailtype" id="emailtype" className='form-control'/>
+                  </div>
+                  <div className='d-grid btn-input'>
+                    <input type={"submit"} value="submit" class="btn text-uppercase" />
+                  </div>
+                </div>
+    
+              </form>
+    
+          </div>
+        </div>
+      </div>
+      
+      
+    </footer>
+  )
+}
+
+
 
 
 function Main(){
@@ -162,6 +407,10 @@ function Main(){
       <NavBar/>
       <WeAreGlint/>
       <WhatWeDo/>
+      <Recentwork/>
+      <OurClients/>
+      <ContactUs/>
+      <Footer/>
     </main>
 
   )
---
+++ src/items.js
@@ -0,0 +1,256 @@
+export const navItems = ["Home","About","Services","Works","Clients","Contacts"];
+
+export const bannerBtns = ["start a project","more about us"];
+
+export const bannerLinks = [
+    {   
+        id : 1 ,
+        title : "facebook",
+        icon : "fab fa-facebook-f ",
+        display : " banner-links-icon"
+    },
+    {
+        id : 2 ,
+        title: "twitter",
+        icon : "fab fa-twitter",
+        display : " banner-links-icon"
+    },
+    {
+        id : 3 ,
+        title : "instagram",
+        icon : "fab fa-instagram",
+        display : "banner-links-icon"
+    },
+    {
+        id : 4 ,
+        title : "youtube",
+        icon : "fab fa-youtube",
+        display : " banner-links-icon"
+    }
+];
+
+var awrads = 250 ,cups = 100, projects = 290 , clients = 36;
+
+
+
+export const counters = [
+    {   
+        id : 1,
+        title : "Awards Received",
+        count : awrads
+    },
+    {
+        id : 2,
+        title : "Cups of Coffee",
+        count : cups
+    },
+    {
+        id : 3,
+        title : "Projects Completed",
+        count : projects
+    },
+    {
+        id : 4,
+        title : "Happy Clients",
+        count : clients
+    },
+]
+
+export const whatwedos = [
+    {
+        id: 1,
+        icon : "fas fa-paint-roller",
+        title : "Brand Identity",
+        para : "Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium. Quo delectus est. Maiores voluptas ab sit natus veritatis ut. Debitis nulla cumque veritatis. Sunt suscipit voluptas ipsa in tempora esse soluta sint."
+    },
+    {
+        id: 2,
+        icon : "fas fa-vector-square",
+        title : "Illustration",
+        para : "Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium. Quo delectus est. Maiores voluptas ab sit natus veritatis ut. Debitis nulla cumque veritatis. Sunt suscipit voluptas ipsa in tempora esse soluta sint."
+    },
+    {
+        id: 3,
+        icon : "fas fa-bullhorn",
+        title : "Marketing",
+        para : "Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium. Quo delectus est. Maiores voluptas ab sit natus veritatis ut. Debitis nulla cumque veritatis. Sunt suscipit voluptas ipsa in tempora esse soluta sint."
+    },
+    {
+        id: 4,
+        icon : "fas fa-globe-europe",
+        title : "Web Design",
+        para : "Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium. Quo delectus est. Maiores voluptas ab sit natus veritatis ut. Debitis nulla cumque veritatis. Sunt suscipit voluptas ipsa in tempora esse soluta sint."
+    },
+    {
+        id: 5,
+        icon : "fas fa-cube",
+        title : "Packaging Design",
+        para : "Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium. Quo delectus est. Maiores voluptas ab sit natus veritatis ut. Debitis nulla cumque veritatis. Sunt suscipit voluptas ipsa in tempora esse soluta sint."
+    },
+    {
+        id: 6,
+        icon : "fas fa-car-battery",
+        title : "Web Development",
+        para : "Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium. Quo delectus est. Maiores voluptas ab sit natus veritatis ut. Debitis nulla cumque veritatis. Sunt suscipit voluptas ipsa in tempora esse soluta sint."
+    },
+]
+
+export const recentWorks =[
+    {
+        id: 1,
+        img : "https://preview.colorlib.com/theme/glint/images/portfolio/lady-shutterbug.jpg",
+        title : "SHUTTERBUG",
+        type : "Branding",
+    },
+    {
+        id: 2,
+        img : "https://preview.colorlib.com/theme/glint/images/portfolio/woodcraft.jpg",
+        title : "WOODCRAFT",
+        type : "Web Design",
+    },
+    {
+        id: 3,
+        img : "https://preview.colorlib.com/theme/glint/images/portfolio/the-beetle.jpg",
+        title : "THE BEETLE",
+        type : "Web Development",
+    },
+    {
+        id: 4,
+        img : "https://preview.colorlib.com/theme/glint/images/portfolio/gallery/g-grow-green.jpg",
+        title : "GROW GREEN",
+        type : "Branding",
+    },
+    {
+        id: 5,
+        img : "https://preview.colorlib.com/theme/glint/images/portfolio/gallery/g-guitarist.jpg",
+        title : "GUITARIST",
+        type : "Web Design",
+    },
+    {
+        id: 6,
+        img : "https://preview.colorlib.com/theme/glint/images/portfolio/palmeira.jpg",
+        title : "PALMEIRA",
+        type : "Web Design",
+    }
+]
+
+export const logos = [
+    {
+        id : 1,
+        img : "https://preview.colorlib.com/theme/glint/images/clients/apple.png.webp",
+        alt : "apple"
+    },
+    {
+        id : 2,
+        img : "https://preview.colorlib.com/theme/glint/images/clients/atom.png",
+        alt : "atom"
+    },
+    {
+        id : 3,
+        img : "https://preview.colorlib.com/theme/glint/images/clients/blackberry.png",
+        alt : "blackberry"
+    },
+    {
+        id : 4,
+        img : "https://preview.colorlib.com/theme/glint/images/clients/dropbox.png",
+        title : "dropbox"
+    },
+    {
+        id : 5,
+        img : "https://preview.colorlib.com/theme/glint/images/clients/envato.png",
+        alt : "envato"
+    },
+    {
+        id : 6,
+        img : "https://preview.colorlib.com/theme/glint/images/clients/firefox.png",
+        alt : "firefox"
+    },
+    {
+        id : 7,
+        img : "https://preview.colorlib.com/theme/glint/images/clients/joomla.png",
+        alt : "joomla"
+    },
+    {
+        id : 1,
+        img : "https://preview.colorlib.com/theme/glint/images/clients/magento.png",
+        alt : "magento"
+    },
+]
+
+export const forminputs = [
+    {
+        id: 1,
+        type: "text",
+        name : "name",
+        inputid : "name",
+        placeholder : "Your Name"
+    },
+    {
+        id: 1,
+        type: "email",
+        name : "email",
+        inputid : "email",
+        placeholder : "Your Email"
+    },
+    {
+        id: 1,
+        type: "text",
+        name : "subject",
+        inputid : "subject",
+        placeholder : "Your Subject"
+    },
+    {
+        id: 1,
+        type: "text",
+        name : "name",
+        inputid : "name",
+        placeholder : "Your Name"
+    }
+]
+
+export const contentInfos = [
+    {
+        id: 1,
+        title : "Where to Find Us",
+        content : "1600 Amphitheatre Parkway Mountain View, CA 94043 US"
+    },
+    {
+        id: 2,
+        title : "Email Us At",
+        emailone : "contact@glintsite.com",
+        emailtwo : "info@glintsite.com",
+    },
+    {
+        id: 3,
+        title : "Call Us At",
+        phone : "Phone: (+63) 555 1212",
+        mobile : "Mobile: (+63) 555 0100",
+        fax : "Fax: (+63) 555 0101"
+    },
+    {
+        id: 4,
+        facebook : "fab fa-facebook-f ",
+        twitter :"fab fa-twitter",
+        instargram : "fab fa-instagram",
+        youtube : "fab fa-youtube",
+    }
+
+]
+
+
+export const footerItems = [
+    {
+        id : 1,
+        logo : "https://preview.colorlib.com/theme/glint/images/logo-footer.png",
+        content : "Proin eget tortor risus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Nulla porttitor accumsan tincidunt. Nulla porttitor accumsan tincidunt. Quaerat voluptas autem necessitatibus vitae aut.",
+    },
+    {
+        id : 2,
+        title : "Get Notified",
+        content : "Quia quo qui sed odit. Quaerat voluptas autem necessitatibus vitae aut non alias sed quia. Ut itaque enim optio ut excepturi deserunt iusto porro.",
+        emailtype: "email",
+        submittype: "submit",
+        value : "submit"
+
+    }
+]
